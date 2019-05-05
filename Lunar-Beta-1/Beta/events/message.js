68b683bac0e49060a84985a58408cf70989241f8
Banhammer151 <careypjr1993@gmail.com>
2019-05-04 22:27:55 -0500

    pick

--- Lunar-Beta-1/Beta/events/message.js
+++ Lunar-Beta-1/Beta/events/message.js
@@ -1,15 +1,13 @@
 /* eslint-disable linebreak-style */
-// The MESSAGE event runs anytime a message is received
-// Note that due to the binding of client to every event, every event
-// goes `client, other, args` when this function is run.
-const moment = require("moment");
+
+//const moment = require("moment");
 const actions = require("../util/modactions");
 module.exports = (client, message) => {
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
+  function getRandomInt() {
+    return Math.floor(Math.random() * 500);
+  }
   if (message.author.bot) return;
   if (message.guild) {
-    // We'll use the key often enough that simplifying it is worth the trouble.
     const key = `${message.guild.id}-${message.author.id}`;
     actions.checkMention(client, message);
     // Triggers on new users we haven't seen before.
@@ -23,7 +21,7 @@ module.exports = (client, message) => {
     client.warnings.ensure(key, {
       user: message.author.id,
       guild: message.guild.id,
-      warnings: 0      
+      warnings: 0
     });
     client.money.ensure(key, {
       user: message.author.id,
@@ -33,16 +31,19 @@ module.exports = (client, message) => {
     const userkey = `${message.author.id}`;
     client.profile.ensure(userkey, {
       user: message.author.id,
-      name : "john or mary doe",
-      age : "18",
-      gender : "male or female",
-      about : "i'm me",
-      relationship : "single",
-      profileimg : "https://placehold.it/300x250"
+      name: "john or mary doe",
+      age: "18",
+      gender: "male or female",
+      about: "i'm me",
+      relationship: "single",
+      profileimg: "https://placehold.it/300x250"
+    });
+    client.drops.ensure(message.guild.id, {
+      pick: "false"
     });
     // Increment the points and save them.
     client.points.inc(key, "points");
-    
+
     // Calculate the user's current level
     const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, "points")));
 
@@ -56,14 +57,36 @@ module.exports = (client, message) => {
   // Grab the settings for this server from the PersistentCollection
   // If there is no guild, get default conf (DMs)
   const settings = client.getSettings(message.guild);
-  
- 
+  const key = `${message.guild.id}-${message.author.id}`;
+
+
   // For ease of use in commands and functions, we'll attach the settings
   // to the message object, so `message.settings` is accessible.
   message.settings = settings;
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
   if (message.content.match(prefixMention)) {
     return message.reply(`My prefix on this guild is \`${settings.prefix}\``);
+
+  }
+  if (message.content.match("pick") && client.drops.get(message.guild.id, "pick") === "true") {
+    message.delete();
+    
+    const moneygained = getRandomInt();
+    const moneyuser = client.money.get(key, "money") + moneygained;
+    client.money.set(key, moneyuser, "money");
+    message.channel.send(`${message.author.tag} Picked up ${moneygained} they now have ${moneyuser}`).then(msg =>{
+      msg.delete(10000);
+    }).catch(
+      console.log("error")
+    );
+    client.drops.set(message.guild.id, "false", "pick");
+  }
+  if (client.drops.get(message.guild.id, "pick") === "true") {
+    message.reply("I dropped Some Money. Who Will Get it? Type Pick to pick it up");
+  }
+  var d = Math.random() * 100;
+  if (d < 5) {
+    client.drops.set(message.guild.id, "true", "pick");
   }
   // Also good practice to ignore any message that does not start with our prefix,
   // which is set in the configuration file.
@@ -78,6 +101,8 @@ module.exports = (client, message) => {
 
   // Get the user or member's permission level from the elevation
   const level = client.permlevel(message);
+  
+
 
   // Check whether the command, or alias, exist in the collections defined
   // in app.js.
@@ -104,18 +129,18 @@ module.exports = (client, message) => {
   // To simplify message arguments, the author's level is now put on level (not member so it is supported in DMs)
   // The "level" command module argument will be deprecated in the future.
   message.author.permLevel = level;
-  
+
   message.flags = [];
   while (args[0] && args[0][0] === "-") {
     message.flags.push(args.shift().slice(1));
   }
   if (cmd.help.name === "daily") {
     const delay = 8.64e+7;
-    const start = Date.now();
+    // const start = Date.now();
     if (client.daily.has(message.author.id)) {
-      const elapsed = Date.now() - start;
-      const remaining = delay - elapsed;
-      var time = moment(remaining).format("h:mm:ss");
+      //const elapsed = Date.now() - start;
+      //const remaining = delay - elapsed;
+      // var time = moment(remaining).format("h:mm:ss");
       return message.reply("Try again Later");
     }
     // Adds the user to the set so that they can't talk for 2.5 seconds
@@ -123,10 +148,12 @@ module.exports = (client, message) => {
     setTimeout(() => {
       // Removes the user from the set after 2.5 seconds
       client.daily.delete(message.author.id);
-    }, delay);}
-  
-  
+    }, delay);
+  }
+
+
   // If the command exists, **AND** the user has permission, run it.
   client.log("log", `${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`, "CMD");
   cmd.run(client, message, args, level);
+  
 };
