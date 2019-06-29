9a92450c40b13fe6a59c12e6429c5d33f2aeb324
Banhammer151 <careypjr1993@gmail.com>
2019-06-29 11:03:19 -0500

    updated join/leave log
    Co-authored-by: Uni <34154200+Uni-Nugiolu@users.noreply.github.com>

--- Lunar-Beta-1/Beta/src/listeners/guildCreate.js
+++ Lunar-Beta-1/Beta/src/listeners/guildCreate.js
@@ -1,7 +1,9 @@
 /* eslint-disable linebreak-style */
 exports.run = async (guild) => {
   const sql = require("sqlite");
+  const now = Date.now();
   sql.open("./database.sqlite");
-  console.log(`I have joined the guild ${guild.name}`);
   sql.run("INSERT OR IGNORE INTO settings (guildid) VALUES (?)", [guild.id]);
+  console.log(`Lunar has Joined ${guild.name}` );
+  this.client.guilds.get("555894246717259797").channels.get("594557392541188096").send(`Lunar has Joined ${guild.name} at ${now}`);
 };
--- Lunar-Beta-1/Beta/src/listeners/guildDelete.js
+++ Lunar-Beta-1/Beta/src/listeners/guildDelete.js
@@ -2,6 +2,8 @@
 exports.run = async (guild) => {
   const sql = require("sqlite");
   sql.open("./database.sqlite");
-  console.log(`I have left the guild ${guild.name}`);
   sql.run("DELETE * FROM settings WHERE guildid = ?", [guild.id]);
+  const now = Date.now();
+  console.log(`Lunar has Left ${guild.name}` );
+  this.client.guilds.get("555894246717259797").channels.get("594557392541188096").send(`Lunar has Left ${guild.name} at ${now}`);
 };
